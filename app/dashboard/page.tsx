2bac2fdf0165a24b65fb48128149709ed2e6899d
songa1 <songachillethe1her@gmail.com>
2024-06-22 18:05:17 +0200

    Solved problems in /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
-"use client";
 
 import DashboardHome from "@/components/Dashboard/DashboardHome";
 import TopTitle from "@/components/Dashboard/TopTitle";
-import React from "react";
 
 function page() {
   return (
