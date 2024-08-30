b1bb78ef30ea287c2b20f21458a7a1ddcf5ea090
songa1 <songachillethe1her@gmail.com>
2024-08-30 02:02:44 +0200

    changed config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  devIndicators: {
-    buildActivity: true,
-  },
-  output: "standalone",
-  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
