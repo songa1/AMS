7f510793da001c75446febb26f95b8c6ff30ddbf
songa1 <songachillethe1her@gmail.com>
2025-01-13 05:08:52 +0200

    I have added new changes on notifications page

--- components/Dashboard/Communications/Notifications.tsx
+++ components/Dashboard/Communications/Notifications.tsx
@@ -8,12 +8,23 @@ import {
   useOpenNotificationMutation,
 } from "@/lib/features/notificationSlice";
 import { getUser } from "@/helpers/auth";
+import {
+  Box,
+  List,
+  ListItemButton,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from "@mui/material";
+import { SectionTitle } from "@/components/Other/TopTitle";
+import Loading from "@/app/loading";
 
 function Notifications() {
   dayjs.extend(relativeTime);
   const [notifications, setNotifications] = useState<any>([]);
   const [currentNotification, setCurrentNotification] = useState<string>();
   const [notification, setNotification] = useState<string | null>();
+  const [loading, setLoading] = useState(false);
   const user = getUser();
 
   const { data, refetch } = useNotificationsQuery(user?.id);
@@ -34,16 +45,32 @@ function Notifications() {
   }, [currentNotification, data, nots]);
 
   const handleOpenNotification = async (id: string) => {
+    setLoading(true);
     await openNotification({ id: id });
     refetch();
     setCurrentNotification(id);
+    setLoading(false);
   };
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="notifications">
-      <div className="notifications-left">
-        <h1 className="noti-sticky-header">Inbox</h1>
-        <ul className="flex flex-col gap-1">
+    <Box className="notifications">
+      <Box className="notifications-left">
+        <SectionTitle title="Inbox" />
+        <List
+          className="flex flex-col gap-1"
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Choose a notification to view
+            </ListSubheader>
+          }
+        >
           {notifications ? (
             [...notifications]
               .sort(
@@ -52,48 +79,51 @@ function Notifications() {
                   new Date(a.createdAt).getTime()
               )
               .map((noti: any, index: number) => (
-                <li
+                <ListItemButton
                   key={index + 1}
-                  className="border-b border-gray-200 p-2 px-4 cursor-pointer hover:bg-blue-100"
+                  selected={openNotification == noti?.id}
                   onClick={() => handleOpenNotification(noti?.id)}
                 >
-                  <div>
-                    <p className="text-xs text-mainBlue">
-                      {dayjs(noti?.createdAt).fromNow()}
-                    </p>
-                    <p className={`${!noti?.opened ? "font-bold" : ""}`}>
-                      {noti.title}
-                    </p>
-                  </div>
-                </li>
+                  <ListItemText
+                    sx={{ fontWeight: !noti?.opened ? 600 : 100 }}
+                    primary={noti.title}
+                    secondary={dayjs(noti?.createdAt).fromNow()}
+                  />
+                </ListItemButton>
               ))
           ) : (
-            <p className="py-4 px-2 text-center text-xs">
+            <Typography
+              variant="body2"
+              className="py-4 px-2 text-center text-xs"
+            >
               No Notifications for Now! Check back later!
-            </p>
+            </Typography>
           )}
-        </ul>
-      </div>
-      <div className="notifications-right">
-        <div className="noti-sticky-header">
-          {currentNotification
-            ? `${
-                notifications.find(
-                  (noti: any) => noti.id == currentNotification
-                )?.title
-              }`
-            : "No notification selected!"}
-        </div>
+        </List>
+      </Box>
+      <Box className="notifications-right">
+        <SectionTitle
+          title={
+            currentNotification
+              ? `${
+                  notifications.find(
+                    (noti: any) => noti.id == currentNotification
+                  )?.title
+                }`
+              : "No notification selected!"
+          }
+        />
         {currentNotification && notification && (
-          <div
+          <Typography
             className="p-5 flex flex-col gap-5 notification"
+            variant="body2"
             dangerouslySetInnerHTML={{
               __html: notification,
             }}
-          ></div>
+          ></Typography>
         )}
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
 
